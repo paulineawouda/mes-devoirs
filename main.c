91da68012721e17a9426b7df878de4aa9be06cf9
uniprstud14 <larrissapauline.awouda@studenti.unipr.it>
2025-03-13 19:20:08 +0100

    Déplacer les fichiers dans EsercizioSoker et supprimer les fichiers non nécessaires

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "fast_trigo.h"
-#include "test_func.h"
-#include "util.h"
-
-int main() {
-    test_trigo_functions();
-    return 0;
-}
--- test_func.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_FUNC_H
-#define TEST_FUNC_H
-
-void test_trigo_functions(void);
-
-#endif // TEST_FUNC_H
