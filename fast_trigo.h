1a2f13a700f9124a530e95884bbcd67c31a2e42f
uniprstud14 <larrissapauline.awouda@studenti.unipr.it>
2025-03-14 11:48:11 +0100

    Réorganisation du projet : suppression des fichiers hors des dossiers

--- fast_trigo.h
+++
@@ -1,8 +0,0 @@
-#ifndef FAST_TRIGO_H
-#define FAST_TRIGO_H
-
-double fast_sin(double x);
-double fast_cos(double x);
-
-#endif // FAST_TRIGO_H
-
--- util.c
+++
@@ -1,6 +0,0 @@
-#include "util.h"
-#include <stdio.h>
-
-void print_result(double result) {
-    printf("Result: %f\n", result);
-}
